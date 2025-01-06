83f924407877b44e0419693561d39b577f156a9d
alsqja <alsqha2626@gmail.com>
2025-01-06 10:44:10 +0900

    feat: login 기능 추가

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { TextField } from "../../components/TextField";
+import { useCallback, useEffect, useState } from "react";
+import { useLogin } from "../../hooks/session";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginReq, loginRes] = useLogin();
+
+  const handleLogin = useCallback(() => {
+    loginReq(email, password);
+  }, [loginReq, email, password]);
+
+  useEffect(() => {
+    if (loginRes.called && loginRes.data) {
+      console.log(loginRes.data.data.accessToken);
+    }
+  }, [loginRes]);
+
+  return (
+    <Wrapper>
+      <Container>
+        <Title>로그인</Title>
+        <TextField title="이메일" value={email} onChange={setEmail} />
+        <TextField
+          title="비밀번호"
+          type="password"
+          value={password}
+          onChange={setPassword}
+        />
+        <LoginBtn onClick={handleLogin}>로그인</LoginBtn>
+        <div>
+          아이디가 없으신가요?
+          <span
+            style={{ color: "#828282", marginLeft: "5px", cursor: "pointer" }}
+          >
+            회원가입
+          </span>
+        </div>
+      </Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f3f3f3;
+`;
+
+const Container = styled.div`
+  width: 500px;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: white;
+`;
+
+const Title = styled.span`
+  font-size: 30px;
+`;
+
+const LoginBtn = styled.div`
+  width: 320px;
+  height: 45px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  border: 1px solid black;
+  margin-top: 30px;
+  margin-bottom: 20px;
+`;
