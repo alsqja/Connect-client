d74c4ce266d527eb15f9da44338f4e254c689015
조현지 <qndjddl001@gmail.com>
2025-01-09 15:30:35 +0900

    feat: 포인트 충전 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import { PointPage } from "./pages/Point";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/globalStyle.css'
 import { AdminUser } from "./pages/AdminUser";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin/user" element={<AdminUser />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/admin/user" element={<AdminUser/>}/>
+        <Route path="/point" element={<PointPage/>}/>
       </Routes>
     </BrowserRouter>
   );
