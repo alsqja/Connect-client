c9ae41e6b0399746f7757df068bddc317043d892
alsqja <alsqha2626@gmail.com>
2025-01-14 19:09:59 +0900

    fix header shadow

--- src/components/Layout/User/UserHeader.tsx
+++ src/components/Layout/User/UserHeader.tsx
@@ -62,7 +62,7 @@ export const UserHeader = () => {
 const Wrapper = styled.div`
   width: 100%;
   height: 60px;
-  border-bottom: 1px solid black;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
   position: fixed;
   background-color: white;
   top: 0;
