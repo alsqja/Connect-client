d31c61a8624ec02fd49e355bdb0febc19a0a7360
alsqja <alsqha2626@gmail.com>
2025-01-31 15:33:08 +0900

    feat: 회원 탈퇴 구

--- src/pages/UserMy/Profile.tsx
+++ src/pages/UserMy/Profile.tsx
@@ -1,7 +1,11 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { ProfileData, UpdateUserData } from "./data";
-import { useGetProfile, useUpdateProfile } from "../../hooks/userApi";
+import {
+  useDeleteUser,
+  useGetProfile,
+  useUpdateProfile,
+} from "../../hooks/userApi";
 import { uploadFile } from "../../hooks/fileApi";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
@@ -22,6 +26,12 @@ export const Profile = () => {
   const [imageFile, setImageFile] = useState<File | null>(null);
   const navigate = useNavigate();
   const [user, setUser] = useRecoilState(userState);
+  const [deleteReq, deleteRes] = useDeleteUser();
+
+  // 회원탈퇴 모달 열림/닫힘 상태
+  const [showWithdrawModal, setShowWithdrawModal] = useState(false);
+  // 모달 내부 비밀번호 상태
+  const [withdrawPassword, setWithdrawPassword] = useState("");
 
   useEffect(() => {
     getReq();
@@ -51,6 +61,7 @@ export const Profile = () => {
     }
   }, [getRes]);
 
+  /** 이미지 업로드 핸들러 */
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
@@ -68,6 +79,7 @@ export const Profile = () => {
     }
   };
 
+  /** 드래그 앤 드롭 */
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
@@ -90,6 +102,7 @@ export const Profile = () => {
     event.preventDefault();
   };
 
+  /** 프로필 수정 핸들러 */
   const handleSubmit = useCallback(async () => {
     if (!updateValues) {
       alert("변경 정보를 입력해주세요");
@@ -98,15 +111,14 @@ export const Profile = () => {
 
     if (imageFile) {
       const result = await uploadFile(imageFile);
-
       const profileUrl = process.env.REACT_APP_IMAGE_URL + result;
-
       updateReq({ ...updateValues, profileUrl });
     } else {
       updateReq(updateValues);
     }
   }, [imageFile, updateReq, updateValues]);
 
+  /** 피드보기 이동 */
   const handleNavigate = useCallback(
     (e: React.MouseEvent) => {
       e.stopPropagation();
@@ -115,130 +127,199 @@ export const Profile = () => {
     [navigate, profile?.id]
   );
 
+  /** 회원탈퇴 버튼 클릭 -> 모달 열기 */
+  const handleWithdrawalClick = useCallback(() => {
+    setShowWithdrawModal(true);
+  }, []);
+
+  /** 회원탈퇴 모달 내 "확인" 버튼 -> 실제 탈퇴 로직 처리 */
+  const handleWithdrawConfirm = useCallback(() => {
+    if (withdrawPassword === "회원 탈퇴") {
+      deleteReq();
+    } else {
+      alert("회원 탈퇴를 입력해 주세요.");
+    }
+  }, [deleteReq, withdrawPassword]);
+
+  /** 회원탈퇴 모달 내 "취소" 버튼 -> 모달 닫기 */
+  const handleWithdrawCancel = useCallback(() => {
+    setShowWithdrawModal(false);
+  }, []);
+
+  useEffect(() => {
+    if (deleteRes.called && !deleteRes.error && !deleteRes.loading) {
+      alert("회원 탈퇴 되었습니다.");
+      setUser(null);
+      window.location.replace("/login");
+    }
+  }, [deleteRes.called, deleteRes.error, deleteRes.loading, setUser]);
+
   if (!profile) return <div>로딩 중...</div>;
 
   return (
-    <Container>
-      <ImageWrapper
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-        onClick={() => document.getElementById("fileInput")?.click()}
-      >
-        <ProfileImage
-          src={previewImage || profile.profileUrl}
-          alt="프로필 이미지"
-        />
-        <UploadMessage>
-          클릭하거나 드래그 앤 드롭하여 사진을 변경하세요
-        </UploadMessage>
-        <FileInput
-          type="file"
-          id="fileInput"
-          accept="image/*"
-          onChange={handleImageUpload}
-        />
-        <FeedButtonWrapper>
-          <FeedButton onClick={(e) => handleNavigate(e)}>피드보기</FeedButton>
-        </FeedButtonWrapper>
-      </ImageWrapper>
-      <InfoWrapper>
-        <SubscriptionInfo>
-          <div>
-            <strong>구독 상태</strong>
-            {profile.expiredDate ? (
-              <SubscriptionText>
-                현재 구독은{" "}
-                <SubscriptionDate>{profile.expiredDate}</SubscriptionDate>일
-                만료됩니다.
-              </SubscriptionText>
-            ) : (
-              <SubscriptionText>구독 정보가 없습니다.</SubscriptionText>
-            )}
-          </div>
-          <div>
-            <strong>소유 포인트 : </strong>
-            {profile.point} P
-          </div>
-        </SubscriptionInfo>
-        <ProfileForm>
-          <ProfileItem>
-            <Label>이메일</Label>
-            <Value>{profile.email}</Value>
-          </ProfileItem>
-          <ProfileItem>
-            <Label>생일</Label>
-            <Value>
-              {profile.birth.slice(0, 4)}년 {profile.birth.slice(4, 6)}월{" "}
-              {profile.birth.slice(6)}일
-            </Value>
-          </ProfileItem>
-          <ProfileItem>
-            <Label>성별</Label>
-            <Value>{profile.gender === "WOMAN" ? "여자" : "남자"}</Value>
-          </ProfileItem>
-          <ProfileItem>
-            <Label>매칭 On/Off</Label>
-            <ToggleSwitch>
-              <input
-                type="checkbox"
-                checked={updateValues?.isActiveMatching}
-                onChange={() =>
-                  setUpdateValues((prev) =>
-                    prev
-                      ? { ...prev, isActiveMatching: !prev.isActiveMatching }
-                      : null
+    <>
+      <Container>
+        <ImageWrapper
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onClick={() => document.getElementById("fileInput")?.click()}
+        >
+          <ProfileImage
+            src={previewImage || profile.profileUrl}
+            alt="프로필 이미지"
+          />
+          <UploadMessage>
+            클릭하거나 드래그 앤 드롭하여 사진을 변경하세요
+          </UploadMessage>
+          <FileInput
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+          <FeedButtonWrapper>
+            <FeedButton onClick={handleNavigate}>피드보기</FeedButton>
+          </FeedButtonWrapper>
+        </ImageWrapper>
+
+        <InfoWrapper>
+          <SubscriptionInfo>
+            <div>
+              <strong>구독 상태</strong>
+              {profile.expiredDate ? (
+                <SubscriptionText>
+                  현재 구독은{" "}
+                  <SubscriptionDate>{profile.expiredDate}</SubscriptionDate>일
+                  만료됩니다.
+                </SubscriptionText>
+              ) : (
+                <SubscriptionText>구독 정보가 없습니다.</SubscriptionText>
+              )}
+            </div>
+            <div>
+              <strong>소유 포인트 : </strong>
+              {profile.point} P
+            </div>
+          </SubscriptionInfo>
+
+          <ProfileForm>
+            <ProfileItem>
+              <Label>이메일</Label>
+              <Value>{profile.email}</Value>
+            </ProfileItem>
+            <ProfileItem>
+              <Label>생일</Label>
+              <Value>
+                {profile.birth.slice(0, 4)}년 {profile.birth.slice(4, 6)}월{" "}
+                {profile.birth.slice(6)}일
+              </Value>
+            </ProfileItem>
+            <ProfileItem>
+              <Label>성별</Label>
+              <Value>{profile.gender === "WOMAN" ? "여자" : "남자"}</Value>
+            </ProfileItem>
+            <ProfileItem>
+              <Label>매칭 On/Off</Label>
+              <ToggleSwitch>
+                <input
+                  type="checkbox"
+                  checked={updateValues?.isActiveMatching}
+                  onChange={() =>
+                    setUpdateValues((prev) =>
+                      prev
+                        ? { ...prev, isActiveMatching: !prev.isActiveMatching }
+                        : null
+                    )
+                  }
+                />
+                <ToggleLabel>
+                  {updateValues?.isActiveMatching ? "On" : "Off"}
+                </ToggleLabel>
+              </ToggleSwitch>
+            </ProfileItem>
+            <ProfileItem>
+              <Label>이름</Label>
+              <Input
+                type="text"
+                defaultValue={updateValues?.name || ""}
+                onChange={(e) =>
+                  setUpdateValues((p) =>
+                    p ? { ...p, name: e.target.value } : null
                   )
                 }
               />
-              <ToggleLabel>
-                {updateValues?.isActiveMatching ? "On" : "Off"}
-              </ToggleLabel>
-            </ToggleSwitch>
-          </ProfileItem>
-          <ProfileItem>
-            <Label>이름</Label>
-            <Input
+            </ProfileItem>
+            <ProfileItem>
+              <Label>변경된 비밀번호</Label>
+              <Input
+                type="password"
+                placeholder="새 비밀번호 입력"
+                value={updateValues?.newPassword || ""}
+                onChange={(e) =>
+                  setUpdateValues((p) =>
+                    p ? { ...p, newPassword: e.target.value } : null
+                  )
+                }
+              />
+            </ProfileItem>
+            <ProfileItem>
+              <Label>비밀번호 확인</Label>
+              <Input
+                type="password"
+                placeholder="비밀번호 확인 입력"
+                value={updateValues?.oldPassword || ""}
+                onChange={(e) =>
+                  setUpdateValues((p) =>
+                    p ? { ...p, oldPassword: e.target.value } : null
+                  )
+                }
+              />
+            </ProfileItem>
+
+            {/* 프로필 수정 버튼 */}
+            <SubmitButton onClick={handleSubmit}>프로필 수정</SubmitButton>
+
+            {/* 회원탈퇴 버튼 */}
+            <WithdrawalButton onClick={handleWithdrawalClick}>
+              회원탈퇴
+            </WithdrawalButton>
+          </ProfileForm>
+        </InfoWrapper>
+      </Container>
+
+      {/* 회원탈퇴 모달 */}
+      {showWithdrawModal && (
+        <ModalOverlay onClick={() => setShowWithdrawModal(false)}>
+          <ModalWrapper onClick={(e) => e.stopPropagation()}>
+            <CloseButton onClick={() => setShowWithdrawModal(false)}>
+              X
+            </CloseButton>
+            <ModalTitle>회원 탈퇴</ModalTitle>
+            <p>정말 탈퇴하시겠습니까? 탈퇴하려면 "회원 탈퇴"를 입력해주세요.</p>
+            <PasswordInput
               type="text"
-              defaultValue={updateValues?.name || ""}
-              onChange={(e) =>
-                setUpdateValues((p) =>
-                  p ? { ...p, name: e.target.value } : null
-                )
-              }
+              placeholder="회원 탈퇴"
+              value={withdrawPassword}
+              onChange={(e) => setWithdrawPassword(e.target.value)}
             />
-          </ProfileItem>
-          <ProfileItem>
-            <Label>변경된 비밀번호</Label>
-            <Input
-              type="password"
-              placeholder="새 비밀번호 입력"
-              value={updateValues?.newPassword || ""}
-              onChange={(e) =>
-                setUpdateValues((p) =>
-                  p ? { ...p, newPassword: e.target.value } : null
-                )
-              }
-            />
-          </ProfileItem>
-          <ProfileItem>
-            <Label>비밀번호 확인</Label>
-            <Input
-              type="password"
-              placeholder="비밀번호 확인 입력"
-              value={updateValues?.oldPassword || ""}
-              onChange={(e) =>
-                setUpdateValues((p) =>
-                  p ? { ...p, oldPassword: e.target.value } : null
-                )
-              }
-            />
-          </ProfileItem>
-          <SubmitButton onClick={handleSubmit}>프로필 수정</SubmitButton>
-        </ProfileForm>
-      </InfoWrapper>
-    </Container>
+
+            <ButtonGroup>
+              <ConfirmButton
+                onClick={handleWithdrawConfirm}
+                disabled={withdrawPassword !== "회원 탈퇴"}
+              >
+                확인
+              </ConfirmButton>
+              <CancelButton onClick={handleWithdrawCancel}>취소</CancelButton>
+            </ButtonGroup>
+          </ModalWrapper>
+        </ModalOverlay>
+      )}
+    </>
   );
 };
+
+/* --------------------- 스타일 --------------------- */
 
 const Container = styled.div`
   display: flex;
@@ -372,6 +453,20 @@ const SubmitButton = styled.button`
   }
 `;
 
+/** 회원탈퇴 버튼 */
+const WithdrawalButton = styled.button`
+  background-color: #dc3545;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 6px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #c82333;
+  }
+`;
+
 const FeedButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -392,5 +487,80 @@ const FeedButton = styled.button`
 
   &:hover {
     background-color: #0056b3;
+  }
+`;
+
+/* --------------------- 모달 --------------------- */
+const ModalOverlay = styled.div`
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  background: #fff;
+  width: 400px;
+  max-height: 80vh;
+  overflow-y: auto;
+  border-radius: 8px;
+  position: relative;
+  padding: 20px;
+`;
+
+const CloseButton = styled.div`
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  cursor: pointer;
+  font-size: 18px;
+`;
+
+const ModalTitle = styled.h2`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const PasswordInput = styled.input`
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0 20px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ConfirmButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 16px;
+  border-radius: 6px;
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  opacity: ${({ disabled }) => (disabled ? "0.5" : "1")};
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const CancelButton = styled.button`
+  background-color: #6c757d;
+  color: white;
+  border: none;
+  padding: 10px 16px;
+  border-radius: 6px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #565e64;
   }
 `;
